1f77e8aea010112953a1c468ccd3531210fb2f73
Lucas_Serodio <53245868+L-Serodio@users.noreply.github.com>
2021-04-25 22:55:15 -0300

    wip: 25/04/2021

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import { CreateUserService } from "../services/user/CreateUserService";
+
+
+class UserController {
+    /** 
+    public async create(request: Request, response: Response): Promise<Response> {
+        
+    } */
+}
+
+export { UserController };
